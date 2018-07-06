739dfcb6ab6c7839f88c75c8a8659b61966c7f20
Misha Milovidov <m.milovidov@icloud.com>
2018-07-06 12:48:12 -0600

    Add read time to blog post list

--- src/components/BlogPostList/components/BlogPostListItem/index.js
+++ src/components/BlogPostList/components/BlogPostListItem/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import moment from 'moment';
+import wordCount from 'html-word-count';
 import './styles.css';
 
 const BlogPostListItem = ({blogPost}) => {
   const blogTitle = blogPost.title;
   const blogExcerpt = blogPost.excerpt;
   const blogDate = moment(blogPost.date).locale(navigator.language).format('LL');
+  const readTime = Math.round(wordCount(blogPost.content) / 265);
 
   return (
     <div className="BlogPostListItem">
       <h1 className="title">{blogTitle}</h1>
       <div className="metadata">
-        <span>{blogDate}</span>
+        <div className="date">
+          <span>{blogDate}</span>
+        </div>
+        <div className="read-time">
+          <span>{readTime} Minutes</span>
+        </div>
       </div>
       <div className="excerpt" dangerouslySetInnerHTML={{__html: blogExcerpt}} />
     </div>
