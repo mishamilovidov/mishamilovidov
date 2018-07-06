e562568b47a567b72a7038188ca8b5cf147c2aaf
Misha Milovidov <m.milovidov@icloud.com>
2018-07-05 22:09:20 -0600

    Refactor header component out of app; add styling and markup for header component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">My Site is Coming Soon!</h1>
-        </header>
-        <p className="App-intro">
-          My website is <b>coming soon</b>. In the meantime, feel free to be enchanted by the ReactJS logo.
-        </p>
-        <p className="App-intro">
-          I am testing to see if my CircleCI workflow succeeded by deploying this change.
-        </p>
+        <Header />
+        <h3>Site coming soon...</h3>
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './Header.css';
+import facebook from './icons/facebook.png';
+import instagram from './icons/instagram.png';
+import linkedin from './icons/linkedin.png';
+import twitter from './icons/twitter.png';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="Header-header">
+        <img src="https://www.gravatar.com/avatar/fcb755908eac859bac7ab6632212dd51?s=300" className="Header-logo" alt="profile" />
+        <h1 className="Header-title">Misha Milovidov</h1>
+        <h5 className="Header-subtitle">Software Engineer | Hippopotamus Admirer | Fixed-Gear Cyclist</h5>
+        <div className="Header-icons">
+          <span className="Header-icon">
+            <a href="//www.linkedin.com/in/mmilovidov/" target="_blank" rel="noopener noreferrer">
+              <img src={linkedin} alt="Misha Milovidov's LinkedIn" />
+            </a>
+          </span>
+          <span className="Header-icon">
+            <a href="//www.instagram.com/m_milovidov/" target="_blank" rel="noopener noreferrer">
+              <img src={instagram} alt="Misha Milovidov's Instagram" />
+            </a>
+          </span>
+          <span className="Header-icon">
+            <a href="//twitter.com/m_milovidov" target="_blank" rel="noopener noreferrer">
+              <img src={twitter} alt="Misha Milovidov's Twitter" />
+            </a>
+          </span>
+          <span className="Header-icon">
+            <a href="//www.facebook.com/m.alexeyvich" target="_blank" rel="noopener noreferrer">
+              <img src={facebook} alt="Misha Milovidov's Facebook" />
+            </a>
+          </span>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
