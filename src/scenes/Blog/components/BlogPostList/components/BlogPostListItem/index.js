0aa8f06a54771ac4ff5479650394109ab9b36659
Misha Milovidov <m.alexeyvich@gmail.com>
2018-07-09 14:25:08 -0600

    Add feature image to blog post list

--- src/scenes/Blog/components/BlogPostList/components/BlogPostListItem/index.js
+++ src/scenes/Blog/components/BlogPostList/components/BlogPostListItem/index.js
@@ -4,6 +4,7 @@ import wordCount from 'html-word-count';
 import './styles.css';
 
 const BlogPostListItem = ({blogPost}) => {
+  const blogFeatureImage = blogPost.featured_image;
   const blogTitle = blogPost.title;
   const blogExcerpt = blogPost.excerpt;
   const blogDate = moment(blogPost.date).locale(navigator.language).format('LL');
@@ -11,16 +12,25 @@ const BlogPostListItem = ({blogPost}) => {
 
   return (
     <div className="BlogPostListItem">
-      <h1 className="title">{blogTitle}</h1>
-      <div className="metadata">
-        <div className="date">
-          <span>{blogDate}</span>
+      <div className="tile">
+        <img
+          src={blogFeatureImage}
+          alt={blogTitle}
+          onError={(e)=>{
+            e.target.src="//dummyimage.com/400x400/e5e5e5/e5e5e5.png"
+          }} 
+        />
+        <h1 className="title">{blogTitle}</h1>
+        <div className="metadata">
+          <div className="date">
+            <span>{blogDate}</span>
+          </div>
+          <div className="read-time">
+            <span>{readTime} Minutes</span>
+          </div>
         </div>
-        <div className="read-time">
-          <span>{readTime} Minutes</span>
-        </div>
+        <div className="excerpt" dangerouslySetInnerHTML={{__html: blogExcerpt}} />
       </div>
-      <div className="excerpt" dangerouslySetInnerHTML={{__html: blogExcerpt}} />
     </div>
   );
 };
